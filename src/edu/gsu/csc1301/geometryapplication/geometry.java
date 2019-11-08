3283d7b03ca6d6a58d3dbc5c6082095460eae832
Ariana Tyson <arianatyson@live.com>
2019-11-07 23:11:06 -0500

    Added displayMenu() method and output

--- src/edu/gsu/csc1301/geometryapplication/geometry.java
+++ src/edu/gsu/csc1301/geometryapplication/geometry.java
@@ -1,20 +1,47 @@
-/**
- * 
- */
 package edu.gsu.csc1301.geometryapplication;
 
 /**
- * @author Ari
+ * This program calculates the area of a circle, the area of a rectangle, 
+ * or the area of a triangle based on user input. 
+ * 
+ * 
+ * @author Ariana Tyson
+ * @version 11/6/2019
  *
  */
+
+import java.util.Scanner;
+
 public class geometry {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
+		Scanner in = new Scanner(System.in);
+		
+		//Input
+		
+		
+		//Computation
+		
+		
+		//Output
+		
+		displayMenu();
+		
 
 	}
 
+	public static void displayMenu()
+	{
+		
+		//Output
+		
+		System.out.println("Welcome to the Geometry Calculator Application\n");
+		
+		System.out.println("1. Calculate the Area of a Circle");
+		System.out.println("1. Calculate the Area of a Rectangle");
+		System.out.println("1. Calculate the Area of a Triangle");
+		
+	}
 }
