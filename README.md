9447537c410510cc66dfa70f84b97a3cb379718a
Ariana Tyson <arianatyson@live.com>
2019-11-07 22:55:41 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GeometryApplication
+ This is the fourth programming assignment for CSC 1301. It calculates the area of a chosen shape, based on user input.
---
+++ src/edu/gsu/csc1301/geometryapplication/geometry.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.gsu.csc1301.geometryapplication;
+
+/**
+ * @author Ari
+ *
+ */
+public class geometry {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
